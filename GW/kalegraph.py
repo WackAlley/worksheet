b4608a9e2e49757cff499ca0065151fd4718b99e
alexdesktop <alexyim98@hotmail.com>
2024-11-11 03:45:02 +0100

    Nov 11 ml

--- GW/kalegraph.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3.12
-import kalepy as kale
-import matplotlib.pyplot as plt
-points, density = kale.density(data3, points=None)
-plt.plot(points,density, 'k-', lw=2.0, alpha=0.8, label='KDE')
